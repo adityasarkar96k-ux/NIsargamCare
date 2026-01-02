34a05611d6fd71424555a0d129ed7bf8a62ef966
adityasarkar96k-ux <adityasarkar96k@gmail.com>
2026-01-02 11:20:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ window.addEventListener('scroll', function() {
         navbar.style.boxShadow = "none";
     }
 });
+
+function sendToWhatsApp() {
+    // 1. फॉर्म मधील माहिती घेणे
+    var name = document.getElementById("custName").value;
+    var email = document.getElementById("custEmail").value;
+    var message = document.getElementById("custMsg").value;
+
+    // 2. तुझा WhatsApp नंबर (91 कोडसह)
+    var myNumber = "917385585398"; 
+
+    // 3. मेसेज तयार करणे (New Line साठी %0a वापरले आहे)
+    var whatsappMessage = 
+        "Hello, I want to enquire about Nisargam Soaps." + "%0a" +
+        "-------------------------" + "%0a" +
+        "*Name:* " + name + "%0a" +
+        "*Email:* " + email + "%0a" +
+        "*Message:* " + message;
+
+    // 4. WhatsApp उघडणे
+    var url = "https://wa.me/" + myNumber + "?text=" + whatsappMessage;
+    window.open(url, '_blank').focus();
+}
